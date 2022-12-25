90d3fbb314d1905c19f15c79c7b54981907d7024
unknown <amitkumarjajoo@gmail.com>
2022-12-25 21:27:18 +0530

    bottom navigation bar added

--- lib/view/dashboard/dashboard.dart
+++ lib/view/dashboard/dashboard.dart
@@ -1,7 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:persistent_bottom_nav_bar_v2/persistent-tab-view.dart';
+import 'package:tech_media/res/color.dart';
 import 'package:tech_media/utils/routes/route_name.dart';
 import 'package:tech_media/view_model/services/session_manager.dart';
 
@@ -13,23 +14,69 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  PersistentTabController _controller =
+      PersistentTabController(initialIndex: 0);
+
+  List<PersistentBottomNavBarItem> _navBarsItems() {
+    return [
+      PersistentBottomNavBarItem(
+        icon: Icon(CupertinoIcons.home),
+        activeColorPrimary: AppColors.primaryIconColor,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(CupertinoIcons.chat_bubble),
+        activeColorPrimary: AppColors.primaryIconColor,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(
+          CupertinoIcons.add,
+          color: AppColors.whiteColor,
+        ),
+        activeColorPrimary: AppColors.primaryIconColor,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(
+          CupertinoIcons.bubble_left_bubble_right,
+        ),
+        activeColorPrimary: AppColors.primaryIconColor,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(CupertinoIcons.profile_circled),
+        activeColorPrimary: AppColors.primaryIconColor,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+    ];
+  }
+
+  List<Widget> _buildScreens() {
+    return [
+      Text('Home'),
+      Text('Chat'),
+      Text('Add'),
+      Text('Message'),
+      Text('Profile')
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(SessionController().userId.toString()),
-      actions: [
-        IconButton(onPressed: (){
-          FirebaseAuth auth = FirebaseAuth.instance;
-          auth.signOut().then((value) {
-            SessionController().userId = '';
-            Navigator.pushNamed(context, RouteName.loginView);
-          });
-        }, icon: Icon(Icons.logout_outlined))
-      ],
-      ),
-      body: Column()
-      
-       );
- 
+    return PersistentTabView(
+      context,
+      controller: _controller,
+      screens: _buildScreens(),
+      items: _navBarsItems(),
+      confineInSafeArea: true,
+      backgroundColor: Colors.white, // Default is Colors.white.
+      handleAndroidBackButtonPress: true, // Default is true.
+      resizeToAvoidBottomInset:
+          true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
+      stateManagement: true, // Default is true.
+      hideNavigationBarWhenKeyboardShows: true,
+      navBarStyle: NavBarStyle.style15,
+    );
   }
 }
